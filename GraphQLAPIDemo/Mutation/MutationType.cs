62d2d9ab6e8e9e56bfd2914f469fb0d88e27d357
mainuser <mainuser@DESKTOP-VRTAIJ0>
2022-02-02 08:14:55 -0500

    Looking into redoing the mutations based on 12.4 blog post

---
+++ GraphQLAPIDemo/Mutation/MutationType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQLAPIDemo.Data;
+
+namespace GraphQLAPIDemo.Mutation
+{
+    public partial class MutationType : ObjectType<Mutation>
+    {
+        protected override void Configure(
+            IObjectTypeDescriptor<Mutation> descriptor)
+        {
+            descriptor.Field(f => f.AddBook(default));
+        }
+    }
+
+}
